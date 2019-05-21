e49a4d447966bed78ba9cc16975df6384fef6d2f
Faisal Hayat <faisalrana079@gmail.com>
2019-05-21 15:47:25 +0500

    Angular app with dynamic binding

---
+++ src/app/testing/testing.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testing',
+  templateUrl: './testing.component.html',
+  styleUrls: ['./testing.component.scss']
+})
+export class TestingComponent implements OnInit {
+  name= "This is my testing component."
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
